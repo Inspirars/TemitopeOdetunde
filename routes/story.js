93953a84cb3d5b8057962a342e9c8d43564eedff
oladee <deemajor230600@gmail.com>
2024-05-06 18:57:14 +0100

    fini

--- routes/story.js
+++ routes/story.js
@@ -4,8 +4,8 @@ const asyncHandler = require("express-async-handler")
 
 router.get("/story/:id",asyncHandler(async (req,res,next)=>{
     var story = await Post.findById(req.params.id).exec()
-    var related = await Post.findById({},"title tag date image").exec()
-    res.render("story",{title : "Blog-Story", story, markdown})
+    // var related = await Post.findById({},"title tag date image").exec()
+    res.render("story",{title : "Blog-Story", story})
 }) )
 
 module.exports = router
