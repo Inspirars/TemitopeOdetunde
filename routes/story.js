8a9554a6c4fe26f235963e43a5d8e66b8796ab97
oladee <deemajor230600@gmail.com>
2024-05-06 18:15:11 +0100

    fini

--- routes/story.js
+++ routes/story.js
@@ -1,13 +1,10 @@
 const router = require("express").Router()
 const Post = require("../models/blogModel")
 const asyncHandler = require("express-async-handler")
-var TurndownService = require('turndown')
 
 router.get("/story/:id",asyncHandler(async (req,res,next)=>{
     var story = await Post.findById(req.params.id).exec()
-    var turndownService = new TurndownService()
-    var markdown = turndownService.turndown('<h1>Hello world!</h1>')
-    console.log(markdown)
+    var related = await Post.findById({},"title tag date image").exec()
     res.render("story",{title : "Blog-Story", story, markdown})
 }) )
 
